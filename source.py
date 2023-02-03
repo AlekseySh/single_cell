ad752c33c40debbfb42b09cb2070ad99cb36e37e
alekseysh <lexibender@ya.ry>
2023-02-03 19:03:32 +0100

    upd

--- source.py
+++ source.py
@@ -1,10 +1,12 @@
 from collections import Counter
+from pprint import pprint
 
 import torch.nn
 from oml import const
 from oml.interfaces.datasets import IDatasetWithLabels
 from oml.interfaces.models import IExtractor
 from oml.interfaces.models import IPairwiseModel
+from oml.utils.misc_torch import elementwise_dist, assign_2d, pairwise_dist
 
 
 class BioDatasetWithLabels(IDatasetWithLabels):
@@ -42,6 +44,7 @@ class SimpleExtractor(IExtractor):
     def forward(self, x):
         return self.fc(x)
 
+    @property
     def feat_dim(self):
         return self.fc.out_features
 
@@ -61,7 +64,9 @@ class SimpleSiamese(IPairwiseModel):
     def forward(self, x1, x2):
         x1 = self.extractor1(x1)
         x2 = self.extractor2(x2)
-        x = self.head(torch.concat([x1, x2]))
+        x = torch.concat([x1, x2], dim=-1)
+        x = self.head(x)
+        x = x.view(len(x))
         return x
 
 
@@ -71,10 +76,50 @@ class PairsSamplerTwoModalities:
         super(PairsSamplerTwoModalities, self).__init__()
 
     def sample(self, features_a, features_b, labels_a, labels_b):
-        pass
+        n = len(features_a)
+
+        distances = pairwise_dist(features_a, features_b)
+        is_positive = labels_a.unsqueeze(-1) == labels_b
+
+        distances_pos = distances.clone()
+        distances_pos[~is_positive] = -float("inf")
+        ii_hard_pos = distances_pos.argmax(dim=1)
+
+        distances_neg = distances.clone()
+        distances_neg[is_positive] = +float("inf")
+        ii_hard_neg = distances_neg.argmin(dim=1)
+
+        ii = torch.arange(n)
+
+        ids_1 = torch.concat([ii, ii], dim=0)
+        ids_2 = torch.concat([ii_hard_pos, ii_hard_neg], dim=0)
+
+        is_negative = torch.ones(2 * n).bool()
+        is_negative[:n] = False
+
+        return ids_1, ids_2, is_negative
 
 
-features_a = torch.randn((4, 10))
-features_b = torch.randn((4, 10))
-labels_a = torch.tensor([0, 1, 2, 3])
-labels_b = torch.tensor([0, 1, 2, 3])
+def check_miner():
+    labels_a = torch.tensor([0, 1, 2, 3])
+    labels_b = torch.tensor([1, 2, 3, 0])
+
+    features_a = torch.ones((4, 3)) * labels_a.view(-1, 1)
+    features_b = torch.ones((4, 3)) * labels_b.view(-1, 1) + 0.1
+
+    ids_1, ids_2, is_negative = PairsSamplerTwoModalities().sample(
+        features_a,
+        features_b,
+        labels_a,
+        labels_b
+    )
+
+    pprint(list(zip(
+        features_a[ids_1],
+        features_b[ids_2],
+        is_negative
+    )))
+
+
+if __name__ == '__main__':
+    check_miner()
